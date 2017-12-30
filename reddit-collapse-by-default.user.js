8da09dd5d90e893e5b814eda79b8b1292910b195
Paul "Joey" Clark <joey@neuralyte.org>
2017-12-30 13:02:06 +0800

    Do not collapse top-level comments; remove CSS (we don't need it)

--- reddit-collapse-by-default.user.js
+++ reddit-collapse-by-default.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Reddit collapse by default
 // @namespace    http://github.com/Wizek/
-// @version      0.2.1
-// @description  All Reddit comments collapsed by default
+// @version      0.2.2
+// @description  Second level Reddit comments collapsed by default
 // @author       Milan Nagy
+// @contributors joeytwiddle
 // @match        https://www.reddit.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
+/*
 GM_addStyle("                     \
   .comment.collapsed .usertext {  \
     display: block;               \
@@ -19,3 +21,7 @@ GM_addStyle("                     \
 ")
 
 $('.noncollapsed > .entry > .tagline > .expand').click()
+*/
+
+// Do not collapse top-level comments
+$('.noncollapsed .noncollapsed > .entry > .tagline > .expand').click()
